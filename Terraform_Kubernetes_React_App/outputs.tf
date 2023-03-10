4b4f36fb7d68a158885d5d22296fa1e392649fa3
alexprojects1 <89503576+alexprojects1@users.noreply.github.com>
2023-03-10 13:02:39 -0500

    commit

--- Terraform_Kubernetes_React_App/outputs.tf
+++
@@ -1,9 +0,0 @@
-#-----outputs.tf-----
-#====================
-output "k8s-master-Public-IP" {
-  value = module.compute.k8s-master_server_ip
-}
-
-output "k8s-node-Public-IP" {
-  value = module.compute.k8s-node_server_ip
-}
