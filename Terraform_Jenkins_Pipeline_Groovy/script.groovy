4d6c671713058def887ca2a9d03cfa35c021d65e
alexprojects1 <89503576+alexprojects1@users.noreply.github.com>
2023-03-14 07:05:48 -0400

    commit

--- Terraform_Jenkins_Pipeline_Groovy/script.groovy
+++ Terraform_Jenkins_Pipeline_Groovy/script.groovy
@@ -1,14 +1,25 @@
-def buildApp() {
-    echo 'building the application...'
-} 
-
-def testApp() {
-    echo 'testing the application...'
-} 
-
-def deployApp() {
-    echo 'deplying the application...'
-    echo "deploying version ${params.VERSION}"
-} 
-
-return this
+pipeline {
+  parameters{
+      string(name:'parameter1',  defaultValue:''  ?: '')
+      string(name:'parameter2',  defaultValue:''  ?: 'test')
+      string(name:'parameter3',  defaultValue:''  ?: '')
+  }
+  agent any 
+  stages {
+        stage('Build') { 
+            steps {
+                sh 'echo ${parameter1}'
+            }
+        }
+        stage('Test') { 
+            steps {
+                sh 'echo ${parameter2}' 
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                sh 'echo ${parameter3}'
+            }
+        }
+    }
+}
